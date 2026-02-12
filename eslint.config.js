a0bdf3b19015d34d2a76e113fcee6c5669efe818
kullspiser <egorkulik9696@gmail.com>
2026-02-12 23:46:12 +0500

    Fix for build

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    base: command === "build" ? "/opti-tune/" : "/",
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
